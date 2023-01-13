0bc9d89f9bafbda87a2d0a49f26b1f6a83d78e8b
xMichaelRodriguez <scottlovos503@gmail.com>
2023-01-12 19:25:52 -0600

    exercises-topic-3

---
+++ src/Car.java
@@ -0,0 +1,7 @@
+public class Car {
+    public int numberOfDoors;
+
+    public void addDoor(int door){
+        this.numberOfDoors+=door;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        Car coche=new Car();
+        coche.addDoor(2);
+        System.out.println(coche.numberOfDoors);
+
+        System.out.println(suma(10,20,30));
+    }
+
+    public static int suma(int num1, int num2, int num3){
+        return num1+num2+num3;
+    }
+}
